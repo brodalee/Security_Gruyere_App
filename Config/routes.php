52e97319fb8902b3ed55ef49016425edebb91138
Brodalee <boubou-28@live.fr>
2021-05-20 14:10:00 +0200

    Debut route Auth

--- Config/routes.php
+++ Config/routes.php
@@ -12,8 +12,19 @@
  *      ]
  */
 
+use App\Controllers\AuthController;
 use App\Controllers\SimpleController;
 
+$userRepository = (new \App\Model\Entity\User())->getRepository();
+$sauceRepository = (new \App\Model\Entity\Sauce())->getRepository();
+$user_l_d_sauceRepository = (new \App\Model\Entity\UserLikeDislikeSauce())->getRepository();
+
 return [
-    ["path" => "/", "controller" => SimpleController::class, "http_method" => "GET", "controller_method" => "homePage", "name" => "app.home"]
+    ["path" => "/", "controller" => SimpleController::class, "http_method" => "GET", "controller_method" => "homePage", "name" => "app.home"],
+
+    /** Auth **/
+    ["path" => "/signup", "controller" => SimpleController::class, "http_method" => "POST", "controller_method" => "signup", "name" => "app.signup", 'parameters' => [$userRepository]],
+    ["path" => "/login", "controller" => AuthController::class, "http_method" => "POST", "controller_method" => "login", "name" => "app.login", 'parameters' => [$userRepository]],
+
+    /** Connected  **/
 ];
---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\AbstractController;
+use Core\Model\DefaultRepository;
+
+class AuthController extends AbstractController
+{
+
+    public function login(DefaultRepository $userRepository)
+    {
+    }
+
+    public function signup(DefaultRepository $userRepository)
+    {
+    }
+}
