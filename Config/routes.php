0be582792e7f386c58b385df0f041033c106b7e9
Brodalee <boubou-28@live.fr>
2021-05-20 14:17:17 +0200

    Création routes pour les sauces

--- Config/routes.php
+++ Config/routes.php
@@ -13,11 +13,15 @@
  */
 
 use App\Controllers\AuthController;
+use App\Controllers\SauceController;
 use App\Controllers\SimpleController;
+use App\Model\Entity\Sauce;
+use App\Model\Entity\User;
+use App\Model\Entity\UserLikeDislikeSauce;
 
-$userRepository = (new \App\Model\Entity\User())->getRepository();
-$sauceRepository = (new \App\Model\Entity\Sauce())->getRepository();
-$user_l_d_sauceRepository = (new \App\Model\Entity\UserLikeDislikeSauce())->getRepository();
+$userRepository = (new User())->getRepository();
+$sauceRepository = (new Sauce())->getRepository();
+$user_l_d_sauceRepository = (new UserLikeDislikeSauce())->getRepository();
 
 return [
     ["path" => "/", "controller" => SimpleController::class, "http_method" => "GET", "controller_method" => "homePage", "name" => "app.home"],
@@ -27,5 +31,9 @@ return [
     ["path" => "/login", "controller" => AuthController::class, "http_method" => "POST", "controller_method" => "login", "name" => "app.login", 'parameters' => [$userRepository]],
     ["path" => "/disconnect", "controller" => AuthController::class, "http_method" => "GET", "controller_method" => "disconnect", "name" => "app.disconnect"],
 
-    /** Connected  **/
+    /** Sauces  **/
+    ["path" => "/sauces", "controller" => SauceController::class, "http_method" => "GET", "controller_method" => "getAll", "name" => "app.sauce.getAll", 'parameters' => [$sauceRepository]],
+    ["path" => "/sauces/:id", "controller" => SauceController::class, "http_method" => "GET", "controller_method" => "getOneById", "name" => "app.sauce.getOne", 'parameters' => [$sauceRepository]],
+    ["path" => "/sauce/delete", "controller" => SauceController::class, "http_method" => "POST", "controller_method" => "create", "name" => "app.sauce.create", 'parameters' => [$sauceRepository]],
+    ["path" => "/sauce/create", "controller" => SauceController::class, "http_method" => "DELETE", "controller_method" => "delete", "name" => "app.sauce.delete", 'parameters' => [$sauceRepository]],
 ];
---
+++ src/Controllers/SauceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\AbstractController;
+
+class SauceController extends AbstractController
+{
+
+    public function getAll()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function delete()
+    {
+    }
+
+    public function getOneById(string $id)
+    {
+    }
+}
